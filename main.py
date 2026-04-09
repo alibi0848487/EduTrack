1c2a33f2b240254cc3b0b65c3dbe265639def0f7
Aleke <152017299+alibi0848487@users.noreply.github.com>
2026-04-10 00:23:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import models, database
 from routers import students, courses, enrollments
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -9,3 +10,10 @@ models.Base.metadata.create_all(bind=database.engine)
 app.include_router(students.router)
 app.include_router(courses.router)
 app.include_router(enrollments.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
