3c8ef4d434b6baabb54684a6e907124bd8fcf6f5
Aleke <152017299+alibi0848487@users.noreply.github.com>
2026-05-03 11:52:01 +0500

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -9,7 +9,8 @@ DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
-    connect_args={"sslmode": "require"}  
+    connect_args={"sslmode": "require"} 
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
