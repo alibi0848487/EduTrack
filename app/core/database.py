0007799e5687aa4166db0c05c612d9d23d6582d7
Aleke <152017299+alibi0848487@users.noreply.github.com>
2026-05-03 11:49:30 +0500

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"}  
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
